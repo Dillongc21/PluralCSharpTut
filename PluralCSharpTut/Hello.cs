2e05f30d7dd41e229ea3f27dfc3b4cc2c7e0a9d4
dillonchristensen <Dillongc21@gmail.com>
2017-11-07 16:52:46 -0700

    Are you sleepy?(1)

---
+++ PluralCSharpTut/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PluralCSharpTut
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Prompt for name
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            //Hello Message
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
