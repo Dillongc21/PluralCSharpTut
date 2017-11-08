e32a17c35f337db81e14b0a391714073cb1e56ad
dillonchristensen <Dillongc21@gmail.com>
2017-11-08 03:05:15 -0700

    Are you sleepy?(2)

--- PluralCSharpTut/Hello.cs
+++ PluralCSharpTut/Hello.cs
@@ -2,16 +2,34 @@
 
 namespace PluralCSharpTut
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //Prompt for name
+
+            // Prompt for name
+
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
 
-            //Hello Message
-            Console.WriteLine("Hello, " + name);
+
+            // Prompt for sleep info
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int sleepHours = int.Parse(Console.ReadLine());
+
+
+            // Hello message
+
+            Console.WriteLine("\nHello, " + name);
+
+
+            // Sleep logic
+
+            if (sleepHours >= 7)
+                Console.WriteLine("You are well rested!");
+            else
+                Console.WriteLine("Get mo' sleep fool!");
         }
     }
 }
