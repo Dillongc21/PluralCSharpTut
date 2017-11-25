c668d31e20f3437984a9f079be2f0f49e7e136df
dillonchristensen <Dillongc21@gmail.com>
2017-11-25 15:25:34 -0800

    Tests and Floats & Summary

--- Grades.Tests/GradebookTests.cs
+++ Grades.Tests/GradebookTests.cs
@@ -15,5 +15,26 @@ namespace Grades.Tests
             GradeStatistics stats = book.ComputeStatistics();
             Assert.AreEqual(stats.HighGrade, 90);
         }
+
+        [Test()]
+        public void ComputesLowGrade()
+        {
+            Gradebook book = new Gradebook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(stats.LowGrade, 10);
+        }
+
+        [Test()]
+        public void ComputesAvgGrade()
+        {
+            Gradebook book = new Gradebook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(stats.AvgGrade, 85.166, 0.001);
+        }
     }
 }
--- PluralCSharpTut/Grades.cs
+++ PluralCSharpTut/Grades.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
+
 namespace Grades
 {
     class Program
