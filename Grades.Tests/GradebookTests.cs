22286edb26750e2182d8df53b0e64e1f8ffba867
dillonchristensen <Dillongc21@gmail.com>
2017-11-25 13:48:37 -0800

    Tests & Access Modifiers

---
+++ Grades.Tests/GradebookTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using Grades;
+namespace Grades.Tests
+{
+    [TestFixture()]
+    public class GradebookTest
+    {
+        [Test()]
+        public void ComputesHighGrade()
+        {
+            Gradebook book = new Gradebook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(stats.HighGrade, 90);
+        }
+    }
+}
