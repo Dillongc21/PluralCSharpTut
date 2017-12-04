ad741be95cd85255392dd07ea70949bd0f44b2a0
Dillon Christensen <dillongc21@gmail.com>
2017-12-04 16:30:11 -0700

    Finally

--- PluralCSharpTut/Program.cs
+++ PluralCSharpTut/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,33 +14,53 @@ namespace Grades
         static void Main(string[] args)
         {
             Gradebook book = new Gradebook();
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
 
-            // Prompt user to enter name
-            try
-            {
-                Console.Write("Enter a name: ");
-                book.Name = Console.ReadLine();
-            }
-            catch(ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch(NullReferenceException)
-            {
-                Console.WriteLine("Something went wrong");
-            }
-
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-            book.WriteGrades(Console.Out);
-
+        private static void WriteResults(Gradebook book)
+        {
             Console.WriteLine(book.Name);
             GradeStatistics stats = book.ComputeStatistics();
             WriteResult("Average Grade", stats.AverageGrade);
             WriteResult("Highest Grade", stats.HighestGrade);
             WriteResult("Lowest Grade", stats.LowestGrade);
             WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(Gradebook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(Gradebook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(Gradebook book)
+        {
+            // Prompt user to enter name
+            try
+            {
+                Console.Write("Enter a name: ");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Something went wrong");
+            }
         }
 
         static void WriteResult(string description, string result)
