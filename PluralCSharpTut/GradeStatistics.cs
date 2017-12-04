e2325f697db64b1c7fa98c00565fa11bd973ac88
Dillon Christensen <dillongc21@gmail.com>
2017-12-04 14:23:36 -0700

    Branching

--- PluralCSharpTut/GradeStatistics.cs
+++ PluralCSharpTut/GradeStatistics.cs
@@ -8,13 +8,44 @@ namespace Grades
 {
     public class GradeStatistics
     {
+        public string LetterGrade {
+            get
+            {
+                string result;
+
+                if(AverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if(AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if(AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if(AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+
+                return result;
+            }
+        }
+
+        public float AverageGrade;
+        public float LowestGrade;
+        public float HighestGrade;
+
         public GradeStatistics()
         {
             HighestGrade = 0;
             LowestGrade = float.MaxValue;
         }
-        public float AverageGrade;
-        public float LowestGrade;
-        public float HighestGrade;
     }
 }
---
+++ PluralCSharpTut/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gradebook book = new Gradebook();
+
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average Grade", stats.AverageGrade);
+            WriteResult("Highest Grade", stats.HighestGrade);
+            WriteResult("Lowest Grade", stats.LowestGrade);
+            WriteResult("Grade", stats.LetterGrade);
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+    }
+}
