adaec5f78df341b89edaf42ef4bf0d30bbb32e21
Dillon Christensen <dillongc21@gmail.com>
2017-12-06 12:30:49 -0700

    Delegate Inference

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -19,8 +19,8 @@ namespace DelegatesAndEvents
             //DoWork(del1);
 
             var worker = new Worker();
-            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
-            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            worker.WorkPerformed += Worker_WorkPerformed;
+            worker.WorkCompleted += Worker_WorkCompleted;
             worker.DoWork(8, WorkType.GenerateReports);
         }
 
