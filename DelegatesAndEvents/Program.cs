2c09770c2e0ed1550ac575811fa089e3a43a103c
Dillon Christensen <dillongc21@gmail.com>
2017-12-06 12:43:11 -0700

    Anonymous Methods

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -19,20 +19,29 @@ namespace DelegatesAndEvents
             //DoWork(del1);
 
             var worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkCompleted += Worker_WorkCompleted;
+
+            worker.WorkPerformed += delegate(object sender, WorkPerformedEventArgs e)
+            {
+                Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
+            };
+
+            worker.WorkCompleted += delegate (object sender, EventArgs e)
+            {
+                Console.WriteLine("Worker is done.");
+            };
+
             worker.DoWork(8, WorkType.GenerateReports);
         }
 
-        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
-        }
+        //private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
+        //}
 
-        private static void Worker_WorkCompleted(object sender, EventArgs e)
-        {
-            Console.WriteLine("Worker is done.");
-        }
+        //private static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Worker is done.");
+        //}
 
         //static void DoWork(WorkPerformedHandler del)
         //{
