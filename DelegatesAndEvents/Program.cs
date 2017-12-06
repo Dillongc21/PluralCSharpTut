f8048545d1df404a43fc952b1053bc7dd10e4641
Dillon Christensen <dillongc21@gmail.com>
2017-12-06 10:46:41 -0700

    Multiple Delegates & Returning a Value w/ Delegate

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType worktype);
+    public delegate int WorkPerformedHandler(int hours, WorkType worktype);
 
     class Program
     {
@@ -14,23 +14,35 @@ namespace DelegatesAndEvents
         {
             WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerfomed1);
             WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerfomed2);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerfomed3);
 
-            DoWork(del2);
+            del1 += del2 + del3;
+
+            DoWork(del1);
         }
 
         static void DoWork(WorkPerformedHandler del)
         {
-            del(5, WorkType.Golf);
+            int finalHours = del(5, WorkType.Golf);
+            Console.WriteLine($"Final Hours: {finalHours}"); // Last delegate wins return type
         }
 
-        static void WorkPerfomed1(int hours, WorkType workType)
+        static int WorkPerfomed1(int hours, WorkType workType)
         {
-            Console.WriteLine($"WorkPerformed1 called {hours}");
+            Console.WriteLine($"WorkPerformed1 called- {hours}");
+            return hours + 1;
         }
 
-        static void WorkPerfomed2(int hours, WorkType workType)
+        static int WorkPerfomed2(int hours, WorkType workType)
         {
-            Console.WriteLine($"WorkPerformed2 called {workType.ToString()}");
+            Console.WriteLine($"WorkPerformed2 called- {workType.ToString()}");
+            return hours + 2;
+        }
+
+        static int WorkPerfomed3(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed3 called, silly buns");
+            return hours + 3;
         }
     }
 
