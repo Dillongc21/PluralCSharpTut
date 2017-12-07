06759d1ad4e295fca3063084f294ed7cbbccf71c
Dillon Christensen <dillongc21@gmail.com>
2017-12-07 15:43:35 -0700

    Set up project & built Maybe()

---
+++ EncapSOLID/FileStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapSOLID
+{
+    public class FileStore
+    {
+        public FileStore(string workingDirectory)
+        {
+            this.WorkingDirectory = workingDirectory;
+        }
+
+        public string WorkingDirectory { get; }
+
+        public void Save(int id, string message)
+        {
+            
+        }
+
+        public string GetFileName(int id)
+        {
+            return Path.Combine(this.WorkingDirectory, id + "txt");
+        }
+    }
+}
---
+++ EncapSOLID/Maybe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapSOLID
+{
+    public class Maybe<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> _values;
+
+        public Maybe()
+        {
+            this._values = new T[0];
+        }
+
+        public Maybe(T value)
+        {
+            if (value != null)
+            {
+                this._values = new[] { value };
+            }
+            else
+            {
+                this._values = new T[0];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this._values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
